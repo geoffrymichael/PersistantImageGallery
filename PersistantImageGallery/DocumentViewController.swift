b18d42fbf25b334e5da6cd2521a4395343e4ec1d
Geoffry Gambling <geoffrygambling@Geoffrys-iMac.local>
2019-07-23 17:46:20 -0700

    Initial Commit

---
+++ PersistantImageGallery/DocumentViewController.swift
@@ -0,0 +1,65 @@
+//
+//  DocumentViewController.swift
+//  PersistantImageGallery
+//
+//  Created by Geoffry Gambling on 7/23/19.
+//  Copyright © 2019 Geoffry Gambling. All rights reserved.
+//
+
+import UIKit
+
+class DocumentViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    
+    var higgins = UIImage(named: "higgins")
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "imageCell", for: indexPath) as! ImageCollectionViewCell
+        
+        cell.image = higgins
+        
+        return cell
+    }
+    
+    
+    
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView! {
+        didSet {
+            collectionView.delegate = self
+            collectionView.dataSource = self
+        }
+    }
+    
+    
+    
+    @IBOutlet weak var documentNameLabel: UILabel!
+    
+    var document: UIDocument?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Access the document
+        document?.open(completionHandler: { (success) in
+            if success {
+                // Display the content of the document, e.g.:
+                self.documentNameLabel.text = self.document?.fileURL.lastPathComponent
+            } else {
+                // Make sure to handle the failed import appropriately, e.g., by presenting an error message to the user.
+            }
+        })
+    }
+    
+    @IBAction func dismissDocumentViewController() {
+        dismiss(animated: true) {
+            self.document?.close(completionHandler: nil)
+        }
+    }
+}
---
+++ PersistantImageGallery/ImageCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  ImageCollectionViewCell.swift
+//  PersistantImageGallery
+//
+//  Created by Geoffry Gambling on 7/23/19.
+//  Copyright © 2019 Geoffry Gambling. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+    var magnum = UIImage(named: "magnum")
+    
+    var image: UIImage? {
+        didSet {
+            cellImageView.image = image
+        }
+    }
+    
+    @IBOutlet weak var cellImageView: UIImageView! 
+}
